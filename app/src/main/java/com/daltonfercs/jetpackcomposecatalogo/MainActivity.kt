298172519f10e57f7e9ba71e647fa252427f09b0
Dalton Celadita <daltonfercs@gmail.com>
2024-03-29 21:24:48 -0500

    box

--- app/src/main/java/com/daltonfercs/jetpackcomposecatalogo/MainActivity.kt
+++ app/src/main/java/com/daltonfercs/jetpackcomposecatalogo/MainActivity.kt
@@ -3,13 +3,21 @@ package com.daltonfercs.jetpackcomposecatalogo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.daltonfercs.jetpackcomposecatalogo.ui.theme.JetpackComposeCatalogoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyBox("Android")
                 }
             }
         }
@@ -30,17 +38,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyColum(){
+
+}
+
+@Composable
+fun MyBox(name: String) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.CenterEnd){
+        Box(modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .background(Color.Cyan) ){
+            Text(text = "esto es un ejemplo")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     JetpackComposeCatalogoTheme {
-        Greeting("Android")
+        MyBox("Android")
     }
 }
